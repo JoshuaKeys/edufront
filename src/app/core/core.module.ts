d46e5a3d30920511fae095cdec48800c21dd6337
Aakash <aakash@AAKASHs-MacBook-Pro.local>
2020-05-06 18:24:30 +0530

    introduces architechture principles - phase 1
    * folder structure
    * budget
    * lint

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+  ]
+})
+export class CoreModule { }
