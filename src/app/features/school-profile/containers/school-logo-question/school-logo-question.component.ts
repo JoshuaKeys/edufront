74a013f5a4848f22ca0cd47f81a427efa6264841
Joshua Oguma <joshua_oguma@epam.com>
2020-06-18 00:14:39 +0300

    fixed major issues in school profile

--- src/app/features/school-profile/containers/school-logo-question/school-logo-question.component.ts
+++ src/app/features/school-profile/containers/school-logo-question/school-logo-question.component.ts
@@ -1,5 +1,8 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { SchoolProfileModel } from '../../models/school-profile.model';
+import { clearLogoPreview } from '../../ngrx/actions';
 
 @Component({
   selector: 'edu-school-logo-question',
@@ -10,7 +13,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class SchoolLogoQuestionComponent {
   activatedRouteData = this.activatedRoute.snapshot.data;
   goToDashboard() {
-    this.router.navigateByUrl('/dashboard')
+    this.router.navigateByUrl('/dashboard');
   }
-  constructor(private activatedRoute: ActivatedRoute, private router: Router) { }
+  clearLogoPreview() {
+    this.store.dispatch(clearLogoPreview());
+  }
+  constructor(
+    private store: Store<SchoolProfileModel>,
+    private activatedRoute: ActivatedRoute, private router: Router) { }
 }
