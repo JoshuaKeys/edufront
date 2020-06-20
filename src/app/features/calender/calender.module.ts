ab20f0bf7a07d90c8bf3b4725c1fdcc4234b1ff8
calvin tkm <calvintkmg@gmail.com>
2020-06-20 10:15:52 +0800

    4737-Calender-Setup
    - fixing scrollable,created scrollable directive

--- src/app/features/calender/calender.module.ts
+++ src/app/features/calender/calender.module.ts
@@ -3,20 +3,26 @@ import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { calenderReducer } from './ngrx/reducers';
 import { CalenderRoutingModule } from './calender-routing.module';
-import { AcademicYearQuestionComponent, CalenderShellComponent, SchoolTermsQuestionComponent, TermNamesAndDatesQuestionComponent, VacationNamesAndDatesQuestionComponent, GovernmentHolidaysObservedQuestionComponent } from './containers';
+import {
+  AcademicYearQuestionComponent,
+  CalenderShellComponent,
+  SchoolTermsQuestionComponent,
+  TermNamesAndDatesQuestionComponent,
+  VacationNamesAndDatesQuestionComponent,
+  GovernmentHolidaysObservedQuestionComponent
+} from './containers';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { PreviewParentComponent } from './components/preview-parent/preview-parent.component';
 import { AcademicYearPreviewComponent } from './components/academic-year-preview/academic-year-preview.component';
 import { SchoolTermsPreviewComponent } from './components/school-terms-preview/school-terms-preview.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TermsComponent } from './components/terms/terms.component';
-import { TermsDatesPreviewComponent } from './components/terms-dates-preview/terms-dates-preview.component'
+import { TermsDatesPreviewComponent } from './components/terms-dates-preview/terms-dates-preview.component';
 import { VacationDatesPreviewComponent } from './components/vacation-dates-preview/vacation-dates-preview.component';
 import { HolidayListComponent } from './containers/holiday-list/holiday-list.component';
 import { HolidayAddComponent } from './components/holiday-add/holiday-add.component';
 import { HolidayPillComponent } from './components/holiday-pill/holiday-pill.component';
-
-
+import { SetOVerflowDirective } from './components/set-overflow.directive';
 
 @NgModule({
   declarations: [
@@ -28,13 +34,14 @@ import { HolidayPillComponent } from './components/holiday-pill/holiday-pill.com
     TermsComponent,
     SchoolTermsQuestionComponent,
     TermNamesAndDatesQuestionComponent,
-    VacationNamesAndDatesQuestionComponent, 
+    VacationNamesAndDatesQuestionComponent,
     GovernmentHolidaysObservedQuestionComponent,
     TermsDatesPreviewComponent,
     VacationDatesPreviewComponent,
     HolidayListComponent,
     HolidayAddComponent,
-    HolidayPillComponent
+    HolidayPillComponent,
+    SetOVerflowDirective
   ],
   imports: [
     CommonModule,
@@ -44,4 +51,4 @@ import { HolidayPillComponent } from './components/holiday-pill/holiday-pill.com
     StoreModule.forFeature('calendar', calenderReducer)
   ]
 })
-export class CalenderModule { }
+export class CalenderModule {}
---
+++ src/app/features/calender/components/set-overflow.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SetOVerflowDirective } from './set-overflow.directive';
+
+describe('SetOVerflowDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SetOVerflowDirective();
+    expect(directive).toBeTruthy();
+  });
+});
