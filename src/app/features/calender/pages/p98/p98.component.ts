ab203ce4cb76e22b3be3b99f49910f0fda20abee
calvin tkm <calvintkmg@gmail.com>
2020-06-23 16:16:12 +0800

    4737-Calender-Setup
    developed till screen p98

--- src/app/features/calender/pages/p98/p98.component.ts
+++ src/app/features/calender/pages/p98/p98.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ElementRef,
+  ViewChild
+} from '@angular/core';
 @Component({
   selector: 'edu-p98',
   templateUrl: './p98.component.html',
@@ -7,10 +12,42 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class P98Component implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  timeArr = Array(60).fill('');
+
+  popoverArr = Array(12).fill('');
+  classArr = [
+    {
+      break: [0]
+    }
+  ];
+
+  addBreakArr(index) {
+    this.classArr[index].break.push(1);
+  }
+  addClassArr() {
+    this.classArr.push({ break: [0] });
   }
 
+  @ViewChild('scrollableEl') scrollableEl: ElementRef;
+  startScroll(el) {
+    if (typeof this.scrollableEl === 'undefined') {
+      return true;
+    }
+    var vh = Math.max(
+      document.documentElement.clientHeight,
+      window.innerHeight || 0
+    );
+    var scrollableHeight = this.scrollableEl.nativeElement.offsetHeight;
+    var maxHeight = vh - 330;
+    // console.log(
+    //   'maxHeight - ' + maxHeight + ',scrollable - ' + scrollableHeight
+    // );
+    let res = scrollableHeight >= maxHeight - 10;
+    // console.log(res);
+    return res;
+  }
 }
