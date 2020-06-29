2189aaa466edac39beb6d57929cb10cbf8260ccf
calvin tkm <calvin.tan@edufront.co.in>
2020-06-29 16:03:23 +0800

    phase-3
    reseted routes and auth

--- src/app/features/calender/route-config/index.ts
+++ src/app/features/calender/route-config/index.ts
@@ -97,9 +97,9 @@ export const calenderNavigation = {
 };
 
 export const calenderRouteToComponentMap = {
-  // 'dates-of-academic-year': fromCalenderContainer.AcademicYearQuestionComponent,
+  'dates-of-academic-year': fromCalenderContainer.AcademicYearQuestionComponent,
   // 'dates-of-academic-year': fromPages.P101Component,
-  'dates-of-academic-year': PreviewTestComponent,
+  // 'dates-of-academic-year': PreviewTestComponent,
 
   'school-terms-question': fromCalenderContainer.SchoolTermsQuestionComponent,
   'term-names-and-dates-question':
