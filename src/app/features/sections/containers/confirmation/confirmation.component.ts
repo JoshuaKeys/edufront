db5543883912b2ca855283f875cc0f67ba00c63e
calvin tkm <calvin.tan@edufront.co.in>
2020-07-13 20:32:12 +0800

    phase-4-calvinsfix
    -fixed styling and functioanlity of completion modal

--- src/app/features/sections/containers/confirmation/confirmation.component.ts
+++ src/app/features/sections/containers/confirmation/confirmation.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { getAggregatedResult, createClassesWithStudents } from '../../ngrx/actions/sections.actions';
+import {
+  getAggregatedResult,
+  createClassesWithStudents
+} from '../../ngrx/actions/sections.actions';
 import { Store } from '@ngrx/store';
 import { SectionsStateModel } from '../../models/sections-state.model';
 import { Observable } from 'rxjs';
@@ -17,24 +20,27 @@ import { selectModalState } from '../../ngrx/selectors';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ConfirmationComponent implements OnInit {
-  activatedRouteData = this.activatedRoute.snapshot.data
+  activatedRouteData = this.activatedRoute.snapshot.data;
   aggregate: Observable<AggregateModel[]>;
   sectionsModalState: Observable<SectionsModalState>;
   ngOnInit(): void {
     this.sectionsModalState = this.store.select(selectModalState);
-    this.aggregate = this.store.select(selectAggregate)
-    this.aggregate.subscribe(console.log)
-    this.store.dispatch(getAggregatedResult())
+    this.aggregate = this.store.select(selectAggregate);
+    this.aggregate.subscribe(console.log);
+    this.store.dispatch(getAggregatedResult());
   }
   goToDashboard() {
     this.router.navigateByUrl('/dashboard');
   }
+  goToCalendar() {
+    this.router.navigateByUrl('/calendar/dates-of-academic-year');
+  }
   create() {
-    this.store.dispatch(createClassesWithStudents())
+    this.store.dispatch(createClassesWithStudents());
   }
   constructor(
     private activatedRoute: ActivatedRoute,
     private store: Store<SectionsStateModel>,
     private router: Router
-  ) { }
+  ) {}
 }
