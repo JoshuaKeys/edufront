6dc51cf093b09a3a0e92c255df6632bff2b3d11d
calvin tkm <calvin.tan@edufront.co.in>
2020-07-30 18:30:10 +0800

    EDU4997-sections-auto-assing
    finished algorithm, need to test and integrate with store

--- src/app/shared/services/utils.service.ts
+++ src/app/shared/services/utils.service.ts
@@ -17,5 +17,38 @@ export class UtilsService {
       subjects?: any[];
     }[],
     subjects: { gender: string }[]
-  ) {}
+  ) {
+    let sectionIdsArr = sections.map(section => section.sectionIdx);
+    sectionIdsArr.sort();
+    let results = {};
+
+    sectionIdsArr.forEach(sectionId => {
+      results[sectionId] = [];
+    });
+    let maleSubjects = [];
+    let femaleSubjects = [];
+
+    subjects.forEach(subject => {
+      let _subject = JSON.parse(JSON.stringify(subject));
+      if (subject.gender.toLowerCase().trim() == 'male') {
+        maleSubjects.push(_subject);
+      } else {
+        femaleSubjects.push(_subject);
+      }
+    });
+
+    while (maleSubjects.length > 0 || femaleSubjects.length > 0) {
+      sectionIdsArr.forEach(sectionId => {
+        if (maleSubjects.length > 0) {
+          results[sectionId] = [...results[sectionId], maleSubjects.pop()];
+        }
+        if (femaleSubjects.length > 0) {
+          results[sectionId] = [...results[sectionId], femaleSubjects.pop()];
+        }
+      });
+    }
+
+    console.log(subjects, results);
+    //create baskets
+  }
 }
